5ea9d91d8bf5cabf9a5d56922c84ca1756f6a97d
larissadantier <larissa_dantier@hotmail.com>
2021-11-11 17:31:53 -0300

    Home - ignews

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -7,9 +8,20 @@ export default function Home() {
         <title>Home | ignews</title>
       </Head>
 
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋🏻 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </div>
   );
 }
